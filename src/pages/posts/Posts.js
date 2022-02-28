4e203d79606ed3f43f30f0d37370caedee217c22
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-03-01 01:58:47 +0700

    Decorate the post card

--- src/pages/posts/Posts.js
+++ src/pages/posts/Posts.js
@@ -2,31 +2,30 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import PostCard from './components/PostCard'
 
-// Get the posts
-const getPosts = async () => {
-    const result = await axios.get('https://fswd-wp.devnss.com/wp-json/wp/v2/posts')
-    return result.data
-}
-
 function Posts() {
     
     const [posts, setPosts] = useState([])
 
-    useEffect(async () => {
-        setPosts(await getPosts())
-    })
+    useEffect(() => {
+        // Get the posts
+        const getPosts = async () => {
+            const result = await axios.get('https://fswd-wp.devnss.com/wp-json/wp/v2/posts')
+            setPosts(result.data)
+        }
+        getPosts()
+    }, [])
 
     const PostList = () => {
         if (!posts.length) return <></>
         return (
             <div className="grid grid-cols-3 gap-12">
-                {posts.map(post => <PostCard post={post} />)}
+                {posts.map(post => <PostCard key={post.id} post={post} />)}
             </div>
         )
     }
     
     return (
-        <div className="container mx-auto px-8 py-12">
+        <div className="container mx-auto p-8 py-12">
             <PostList />
         </div>
     )
--- src/pages/posts/components/PostCard.js
+++ src/pages/posts/components/PostCard.js
@@ -1,11 +1,36 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 function PostCard(props) {
 
     const post = props.post
 
+    const [media, setMedia] = useState({})
+
+    useEffect(() => {
+        const getMedia = async () => {
+            if (!post.featured_media) return
+            const result = await axios.get(`https://fswd-wp.devnss.com/wp-json/wp/v2/media/${post.featured_media}`)
+            setMedia(result.data)
+        }
+        getMedia()
+    }, [])
+    
+    const PostImage = () => {
+        return (
+            <div className="relative h-56 bg-gray-800 rounded-t-lg overflow-hidden">
+                <img src={media.source_url} className="absolute w-full inset-0 my-auto" />
+            </div>
+        )
+    }
+
     return (
-        <div key={post.id}>
-            <div>
-            {post.title.rendered}
+        <div className="bg-gray-700 shadow-xl rounded-lg flex flex-col">
+            <PostImage />
+            <div className="flex justify-between m-4">
+                <div className="truncate">{post.title.rendered}</div>
+                <Link to={`/post/${post.id}`} className="ml-8 text-sky-300 hover:text-sky-500 duration-100">Read</Link>
             </div>
         </div>
     )
