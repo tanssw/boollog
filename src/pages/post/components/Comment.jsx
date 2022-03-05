53e0e013fe5947ef6b704ea799b3a2bc0043d999
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-03-05 22:00:30 +0700

    Add a static comment box

--- src/pages/post/components/Comment.jsx
+++ src/pages/post/components/Comment.jsx
@@ -4,13 +4,18 @@ function Comment(props) {
 
     let comment = props.comment
 
+    const AuthorName = () => {
+        if (comment.author_name) return <a href={comment.author_url} target="_blank" className="hover:underline">{comment.author_name}</a>
+        return 'Anonymous'
+    }
+
     return (
-        <div className="border dark:border-gray-600 p-8 rounded-lg my-4">
+        <div id={comment.id} className="border dark:border-gray-600 p-8 rounded-lg my-4">
             <div className="flex items-center mb-8">
                 <img src={comment.author_avatar_urls['96']} alt="Comment's Profile Image" className="rounded-full w-16 h-16 mr-6" />
                 <div>
-                    <div className="font-bold text-2xl dark:text-blue-300 mb-1">
-                        <a href={comment.author_url} target="_blank" className="hover:underline">{comment.author_name}</a>
+                    <div className="font-bold text-xl dark:text-blue-300 mb-1">
+                        <AuthorName />
                     </div>
                     <div className="dark:text-gray-400 font-light">
                         {dayjs(comment.date).format('DD MMMM YYYY — hh:mm')}
