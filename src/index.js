d25ce465d11950fbfbd20e6904c3f34a2ea8693f
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-03-10 22:26:04 +0700

    Use Hash router instead

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
     <StrictMode>
-        <BrowserRouter basename="/boollog">
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
     document.getElementById('root')
 )
